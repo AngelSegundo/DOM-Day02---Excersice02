ba75391da2ce0693156c0e80b9353d83e09bc1c4
AngelSegundo <angel.segundo.dev@gmail.com>
2022-11-18 17:45:53 +0100

    Upgrade valid

--- main.js
+++ main.js
@@ -8,14 +8,35 @@ const result = document.querySelector("#result")
 
 
 send.onclick = () => {
-    let customerValue = customer.value
-    let materialTypeValue = materialType.value
-    let measuresValue = measures.value
-    let commentsValue = comments.value
+    let customerValue 
+    let materialTypeValue
+    let measuresValue = ''
+    let commentsValue 
+
+    // if (!customerValue){
+    //     alert('Introduce tu nombre')
+    // }
+
+    customer.value ? customerValue = customer.value : alert('Introduce tunombre')
+    materialType.value ? materialTypeValue = materialType.value : alert('Selecciona material')
+    comments.value ? commentsValue = comments.value + '.' : commentsValue = ''
+
+
+
+    measures.forEach(e=>{
+        if (e.checked){
+            measuresValue += e.value
+        } 
+    })
+
+if(customerValue && materialTypeValue && measuresValue) {
 
     result.textContent = "Hola"
-    result.value = `${customerValue} ha pedido una caja de ${materialTypeValue} con unas dimensiones ${measuresValue}. ${commentsValue}.`
+    result.value = `${customerValue} ha pedido una caja de ${materialTypeValue} con unas dimensiones ${measuresValue}.` + `${commentsValue? commentsValue: ' '}`
 
+} 
+
+   
 
 }
 
